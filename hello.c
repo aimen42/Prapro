08f89a6b5303e607a1d8f9492b225956ba858441
Pranjal Singh <pranjalsingh47185@gmail.com>
2025-11-05 00:24:09 +0530

    first

---
+++ hello.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main (){
+    int a;
+    printf("Enter a Side for the Square : ");
+    scanf("%d",&a);
+    int area=a*a;
+    printf("The Area of Square is :%d\n",area);
+    return 0;
+}
